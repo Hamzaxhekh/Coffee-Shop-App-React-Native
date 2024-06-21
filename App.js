a2af24635a58eb241874ceb9beed6f64d62dd431
hamzaxhekh <hamzakhalid861@gmail.com>
2024-06-21 18:38:48 +0500

    Complete

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, { useEffect } from 'react';
 import BottomTabs from './src/stacks/BottomTabs';
 import Details from './src/screens/Details';
 import Payment from './src/screens/Payment';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
+import SplashScreen from 'react-native-splash-screen';
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
